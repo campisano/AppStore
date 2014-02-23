fe5bb94aae8fbaec3c2e82f34663614b02c9d036
t1t0 <t1t0@tiscali.it>
2014-02-22 22:13:49 -0300

    cleanup

--- js/filters.js
+++ js/filters.js
@@ -1,7 +1,12 @@
-angular.module('phonecatFilters', []).filter('checkmark', function()
-{
-    return function(input)
-    {
-        return input ? '\u2713' : '\u2718';
-    };
-});
+// phonecatFilters module
+var phonecatFilters = angular.module('phonecatFilters', []);
+
+// CheckmarkFilter filter
+phonecatFilters.filter('CheckmarkFilter',
+                        function()
+                        {
+                            return function(input)
+                            {
+                                return input ? '\u2713' : '\u2718';
+                            };
+                        });
--- test/spec/controllersSpec.js
+++ test/spec/controllersSpec.js
@@ -1,27 +1,26 @@
-    describe('PhoneCat controllers', function() {
-     
-     
-    
-    
-    describe('PhoneListCtrl', function(){
-    var scope, ctrl, $httpBackend;
-     
-    // Load our app module definition before each test.
-    beforeEach(module('phonecatApp'));
-     
-    // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
-    // This allows us to inject a service but then attach it to a variable
-    // with the same name as the service.
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('phones/phones.json').
-    respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
-     
-    scope = $rootScope.$new();
-    ctrl = $controller('PhoneListCtrl', {$scope: scope});
-    }));
+describe('PhoneCat controllers', function()
+{
+    describe('PhoneListCtrl', function()
+    {
+        var scope, ctrl, $httpBackend;
+
+        // Load our app module definition before each test.
+        beforeEach(module('phonecatApp'));
+
+        // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+        // This allows us to inject a service but then attach it to a variable
+        // with the same name as the service.
+        beforeEach(inject(function(_$httpBackend_, $rootScope, $controller)
+        {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('phones/phones.json').
+                respond([ {name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+
+            scope = $rootScope.$new();
+                ctrl = $controller('PhoneListCtrl', {$scope: scope});
+        }));
     });
-    
-    
-    });
-    
+
+
+});
+
